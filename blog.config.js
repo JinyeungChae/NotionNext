b1f0d56bda9af26b9b8409dd7392d39b22420edd
JinyeungChae <cowlsdyd@gmail.com>
2026-06-09 13:57:09 +0900

    add: delete theme2

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jinyeung Chae', 
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Hello!',
+  BIO: process.env.NEXT_PUBLIC_BIO || 'A programmer who loves to share.',
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-mu-mocha-48.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
